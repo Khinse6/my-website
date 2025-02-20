3c7a5aa39bc58653462de7ebacb6c5d3027af63b
Khinse6 <henri23.2003@gmail.com>
2025-02-20 17:06:31 +0000

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,15 +5,5 @@ export default defineNuxtConfig({
 	future: {
 		compatibilityVersion: 4
 	},
-	compatibilityDate: "2024-04-03",
-	content: {
-		preview: {
-			api: "https://api.nuxt.studio",
-			gitInfo: {
-				name: "my-website",
-				owner: "Khinse6",
-				url: "https://github.com/Khinse6/my-website"
-			}
-		}
-	}
+	compatibilityDate: "2024-04-03"
 });
