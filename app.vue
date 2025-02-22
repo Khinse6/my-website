6843d945d424c9ce7347a02e4a767074a11e9592
Khinse6 <henri23.2003@gmail.com>
2025-02-22 19:35:59 +0000

    Working on Header

--- app.vue
+++ app.vue
@@ -1,12 +1,25 @@
 <template>
 	<UApp>
-		<UContainer>
+		<UNavigationMenu
+			:items="items"
+			class="w-full justify-center sticky top-0 bg-(--ui-bg) border-b-2 border-(--ui-primary) transition duration-500"
+			:ui="{ list: 'gap-10', link: 'text-base' }"
+		/>
+		<UContainer class="min-h-dvh">
 			<NuxtPage />
 		</UContainer>
 	</UApp>
 </template>
 
 <script setup lang="ts">
+const items = ref([
+	{ label: "Projects", to: "/projects" },
+	{
+		label: "Hobbies",
+		to: "/hobbies",
+		children: [{ label: "League of Legends", to: "/hobbies/league-of-legends" }]
+	}
+]);
 const { data } = await useAsyncData("navigation", () => {
 	return queryCollectionNavigation("content");
 });
---
+++ components/appHeader.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>
+		<UNavigationMenu />
+	</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
